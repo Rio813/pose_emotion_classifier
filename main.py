ea5ba460011c13487e901dc5e85d42219cf54bec
Rio813 <wmy854811839@gmail.com>
2023-03-14 16:56:31 +0800

    add some instructions in README.md

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import predictions.classPred
 
 pred = predictions.classPred.Pred(model_path='epoch395_acc81.25_model.pth.tar', video_name="demo.mp4")
+
 label = pred.generate_predictions(pred.generate_data())
+
 print(label)
